71ae658e37677525047dd8ef0634b9405ad3d19e
YoujungSon <andn1026@gmail.com>
2022-11-21 16:18:23 +0900

    #8.12 Slider part One

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
@@ -16,34 +17,52 @@ const Box = styled(motion.div)`
   border-radius: 40px;
   position: absolute;
   top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-
-const boxVariant = {
-  initial: {
-    opcity: 0,
+const box = {
+  invisible: {
+    x: 500,
+    opacity: 0,
     scale: 0,
   },
   visible: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 1,
+    },
   },
-  leaving: {
-    opcity: 0,
+  exit: {
+    x: -500,
+    opacity: 0,
     scale: 0,
-    y: 50,
+    transition: {
+      duration: 1,
+    },
   },
 };
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextPlese = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  const prevPlese = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
+      <button onClick={prevPlese}>prev</button>
       <AnimatePresence>
-        {showing ? <Box variants={boxVariant} initial='initial' animate='visible' exit='leaving' /> : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box key={i} variants={box} initial='invisible' animate='visible' exit='exit'>
+              {i}
+            </Box>
+          ) : null,
+        )}
       </AnimatePresence>
+      <button onClick={nextPlese}>next</button>
     </Wrapper>
   );
 }
